af1d71718d2b2120666370ee2223ce1159d5ae3d
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-09 14:59:53 +0600

    Add js to export paths

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,27 @@
-export { PACKAGE_NAME } from './package-name';
-export { createContainer } from './create-container';
-export { getCameraPermissions } from './get-permissions';
-export { getBase64FromWebp, convertBlobToBase64 } from './get-base64-from-webp';
-export { takePhoto } from './take-photo';
-export { pickGalleryPhotos } from './pick-photos';
-export { uploadPhotos, type IUploadPhotosOptions } from './upload-photos';
-export { uploadGallery, type IUploadGalleryOptions } from './upload-gallery';
-export { downloadImages } from './download-images';
-export { CapacitorCamera } from './components/camera';
-export { ImageCard } from './components/image-card';
-export { useCamera, type IUseCameraOptions } from './hooks/use-camera';
-export { useGallery, type IUseGalleryOptions } from './hooks/use-gallery';
-export { useContainer } from './hooks/use-container';
-export { usePermissions } from './hooks/use-permissions';
+export { PACKAGE_NAME } from './package-name.js';
+export { createContainer } from './create-container.js';
+export { getCameraPermissions } from './get-permissions.js';
+export { getBase64FromWebp, convertBlobToBase64 } from './get-base64-from-webp.js';
+export { takePhoto } from './take-photo.js';
+export { pickGalleryPhotos } from './pick-photos.js';
+export { uploadPhotos, type IUploadPhotosOptions } from './upload-photos.js';
+export { uploadGallery, type IUploadGalleryOptions } from './upload-gallery.js';
+export { downloadImages } from './download-images.js';
+export { CapacitorCamera } from './components/camera.js';
+export { ImageCard } from './components/image-card.js';
+export { useCamera, type IUseCameraOptions } from './hooks/use-camera.js';
+export { useGallery, type IUseGalleryOptions } from './hooks/use-gallery.js';
+export { useContainer } from './hooks/use-container.js';
+export { usePermissions } from './hooks/use-permissions.js';
 export { type Photo, type ImageOptions, type GalleryPhoto, type GalleryImageOptions } from '@capacitor/camera';
-export { LinkName } from './link-name';
+export { LinkName } from './link-name.js';
 
-import { takePhoto } from './take-photo';
-import { pickGalleryPhotos } from './pick-photos';
-import { uploadPhotos } from './upload-photos';
-import { uploadGallery } from './upload-gallery';
+import { takePhoto } from './take-photo.js';
+import { pickGalleryPhotos } from './pick-photos.js';
+import { uploadPhotos } from './upload-photos.js';
+import { uploadGallery } from './upload-gallery.js';
 
-export {Package,PackageOptions} from './package'
+export {Package,PackageOptions} from './package.js'
 
 export const Camera = {
   takePhoto,
